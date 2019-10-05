32fd552bfe88dcc60cb94179c55be2a7b04ef256
Hassan Khan <hassan.khan@morningstar.com>
2019-10-05 00:17:52 -0500

    added .gitignore and tested shell script

--- Documents/iManage/run.sh
+++ Documents/iManage/run.sh
@@ -1,2 +1,3 @@
+  
 #!/bin/sh
 nohup java -cp /Users/hkhan/Documents/iManage/target/iManage-1.0-SNAPSHOT-jar-with-dependencies.jar com.hassan.iManage.main &
--- Documents/iManage/src/main/java/com/hassan/iManage/main.java
+++ Documents/iManage/src/main/java/com/hassan/iManage/main.java
@@ -4,7 +4,9 @@ import static spark.Spark.*;
 
 public class main {
     public static void main(String[] args){
+        System.out.println("before running");
         get("/hello",(req,res) -> "Hello World");
+        System.out.println("after running");
 
     }
 }
